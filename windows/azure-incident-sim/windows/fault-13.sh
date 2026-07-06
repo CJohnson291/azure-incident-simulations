7b46e56c282265826a1fd5d619078ed0eb1b86e0
CJohnson291 <chrisjohnson291@hotmail.com>
2026-07-06 11:25:56 +0100

    Remove accidentally created files

--- windows/azure-incident-sim/windows/fault-13.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-az vm run-command invoke \
-  --resource-group rg-incidentsim-windows \
-  --name vm-win-app1 \
-  --command-id RunPowerShellScript \
-  --scripts "Stop-Service -Name TermService -Force; Set-Service -Name TermService -StartupType Disabled" \
-  --output none
--- windows/azure-incident-sim/windows/fault-14.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-az vm run-command invoke \
-  --resource-group rg-incidentsim-windows \
-  --name vm-win-app1 \
-  --command-id RunPowerShellScript \
-  --scripts "Set-NetFirewallRule -DisplayName 'Remote Desktop - User Mode (TCP-In)' -Enabled False" \
-  --output none
